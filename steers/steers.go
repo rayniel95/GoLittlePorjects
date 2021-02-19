9be3f97c37260fb9eba06c98d7bace2b2bbef5df
Rainyel <rainyel.ramos@gmail.com>
2021-02-19 16:33:10 -0500

    steers improved

--- steers/steers.go
+++ steers/steers.go
@@ -6,12 +6,12 @@ func steers(sticks int) int {
 	if sticks == 0 {
 		return 0
 	}
-	steer := 0
-	myArray := make([]int8, sticks, sticks)
-	myIndex := 0
+	// steer := 0
+	// myArray := make([]int8, sticks, sticks)
+	// myIndex := 0
 
-	steersA(sticks, false, &steer, myArray, myIndex)
-	return steer
+	// steersA(sticks, false, &steer, myArray, myIndex)
+	return steersB(sticks, false)
 }
 
 // TODO - see overload functions
@@ -33,4 +33,18 @@ func steersA(sticks int, vertical bool, numberSteers *int, myArray []int8, index
 	myArray[index] = 1
 	steersA(sticks-1, true, numberSteers, myArray, index+1)
 	myArray[index] = temp
+}
+
+func steersB(n int, isVertical bool) int {
+	if n == 0 {
+		return 1
+	}
+	cant := 0
+	if isVertical {
+		cant += steersB(n-1, false)
+		return cant
+	}
+	cant += steersB(n-1, false)
+	cant += steersB(n-1, true)
+	return cant
 }

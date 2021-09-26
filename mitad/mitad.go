9f637b8c46249434a4b630b23ca8692b3e0d628c
Rainyel <rainyel.ramos@gmail.com>
2021-09-26 16:09:39 -0400

    added equal method

--- mitad/mitad.go
+++ mitad/mitad.go
@@ -1,1 +1,25 @@
 package mitad
+
+type Node struct {
+	value int
+	sons  []*Node
+	token bool
+}
+
+func (node *Node) equal(other *Node) bool {
+	length_other := 0
+	for _, son := range (*other).sons {
+		if !(*son).token {
+			length_other++
+		}
+	}
+	length_this := 0
+	for _, son := range (*node).sons {
+		if !(*son).token {
+			length_this++
+		}
+	}
+	return length_other == length_this && (*node).value == (*other).value
+}
+
+// TODO - restruncture the workspace for import modules

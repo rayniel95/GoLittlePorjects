248b42fad13bb179ea0550a400c7ba2ead8c8726
Rainyel <rainyel.ramos@gmail.com>
2021-09-26 18:10:26 -0400

    testing for mitad added

--- mitad/mitad_test.go
+++ mitad/mitad_test.go
@@ -1,58 +1,61 @@
 package mitad
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_mitad(t *testing.T) {
-	subtree:= &Node{
+	subtree := &Node{
 		taken: true,
 		value: 4,
 		sons: []*Node{
 			&Node{
 				value: 4,
-				sons: []*Node{},
+				sons:  []*Node{},
 			},
 			&Node{
 				value: 2,
 				sons: []*Node{
 					&Node{
 						value: 1,
-						sons: []*Node{},
+						sons:  []*Node{},
 					},
 					&Node{
 						value: 7,
-						sons: []*Node{},
+						sons:  []*Node{},
 					},
 					&Node{
 						value: 9,
-						sons: []*Node{},
+						sons:  []*Node{},
 					},
 				},
 			},
 		},
 	}
-	
-	tree:= &Node{
+
+	tree := &Node{
 		value: 4,
 		sons: []*Node{
 			&Node{
 				value: 4,
-				sons: []*Node{},
+				sons:  []*Node{},
 			},
 			&Node{
 				value: 2,
 				sons: []*Node{
 					&Node{
 						value: 1,
-						sons: []*Node{},
+						sons:  []*Node{},
 					},
 					subtree,
 					&Node{
 						value: 7,
-						sons: []*Node{},
+						sons:  []*Node{},
 					},
 					&Node{
 						value: 9,
-						sons: []*Node{},
+						sons:  []*Node{},
 					},
 				},
 			},
@@ -81,6 +84,26 @@ func Test_mitad(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := mitad(tt.args.node, tt.args.other); got != tt.want {
 				t.Errorf("mitad() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_solveMitad(t *testing.T) {
+	type args struct {
+		node *Node
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Node
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := solveMitad(tt.args.node); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("solveMitad() = %v, want %v", got, tt.want)
 			}
 		})
 	}

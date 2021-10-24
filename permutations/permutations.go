805f609abcdf7548d6fcc809b5f448e2dd302845
Rainyel <rainyel.ramos@gmail.com>
2021-10-24 15:18:48 -0400

    added permutations with repetition

--- permutations/permutations.go
+++ permutations/permutations.go
@@ -1,7 +1,10 @@
 package permutations
 
+import "fmt"
+
 func permuting(original []int, selected []bool, permuted []int, call int) int {
 	if call >= len(original) {
+		fmt.Println(permuted)
 		return 1
 	}
 	times := 0
@@ -12,6 +15,36 @@ func permuting(original []int, selected []bool, permuted []int, call int) int {
 			times += permuting(original, selected, permuted, call+1)
 			selected[index] = false
 		}
+	}
+	return times
+}
+
+func permuting2(original []int, selected []bool, permuted []int, call int) int {
+	if call >= len(permuted) {
+		fmt.Println(permuted)
+		return 1
+	}
+	times := 0
+	for index := 0; index < len(original); index++ {
+		if !selected[index] {
+			selected[index] = true
+			permuted[call] = original[index]
+			times += permuting2(original, selected, permuted, call+1)
+			selected[index] = false
+		}
+	}
+	return times
+}
+
+func permutingWithRep(original []int, permuted []int, call int) int {
+	if call >= len(permuted) {
+		fmt.Println(permuted)
+		return 1
+	}
+	times := 0
+	for index := 0; index < len(original); index++ {
+		permuted[call] = original[index]
+		times += permutingWithRep(original, permuted, call+1)
 	}
 	return times
 }
--- permutations/permutations_test.go
+++ permutations/permutations_test.go
@@ -35,3 +35,65 @@ func Test_permuting(t *testing.T) {
 		})
 	}
 }
+
+func Test_permuting2(t *testing.T) {
+	type args struct {
+		original []int
+		selected []bool
+		permuted []int
+		call     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test1",
+			args{
+				[]int{1, 2, 3, 4},
+				make([]bool, 4),
+				make([]int, 3),
+				0,
+			},
+			24,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := permuting2(tt.args.original, tt.args.selected, tt.args.permuted, tt.args.call); got != tt.want {
+				t.Errorf("permuting2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_permutingWithRep(t *testing.T) {
+	type args struct {
+		original []int
+		permuted []int
+		call     int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test1",
+			args{
+				[]int{1, 2, 3, 4},
+				make([]int, 3),
+				0,
+			},
+			64,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := permutingWithRep(tt.args.original, tt.args.permuted, tt.args.call); got != tt.want {
+				t.Errorf("permutingWithRep() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

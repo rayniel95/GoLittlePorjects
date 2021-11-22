3b8e68474dbee920be11df112b8c758e585aab7a
Rainyel <rainyel.ramos@gmail.com>
2021-11-21 20:17:22 -0500

    added test case where root not heapifyDown

--- heap/heap_test.go
+++ heap/heap_test.go
@@ -113,6 +113,108 @@ func TestHeapNode_heapifyDownRootTreeValue(t *testing.T) {
 	}
 }
 
+func TestHeapNode_heapifyDownRootTreeValueNotMoved(t *testing.T) {
+	nodes := createNodes(32)
+
+	addLeftSon(nodes[1], nodes[2])
+	addRightSon(nodes[1], nodes[3])
+
+	addLeftSon(nodes[2], nodes[4])
+	addRightSon(nodes[2], nodes[5])
+
+	addLeftSon(nodes[3], nodes[6])
+	addRightSon(nodes[3], nodes[7])
+
+	addLeftSon(nodes[4], nodes[8])
+	addRightSon(nodes[4], nodes[9])
+
+	addLeftSon(nodes[5], nodes[11])
+	addRightSon(nodes[5], nodes[12])
+
+	addLeftSon(nodes[6], nodes[12])
+	addRightSon(nodes[6], nodes[13])
+
+	addLeftSon(nodes[7], nodes[14])
+	addRightSon(nodes[7], nodes[15])
+
+	addLeftSon(nodes[8], nodes[16])
+	addRightSon(nodes[8], nodes[17])
+
+	addLeftSon(nodes[9], nodes[18])
+	addRightSon(nodes[9], nodes[19])
+
+	addLeftSon(nodes[10], nodes[20])
+	addRightSon(nodes[10], nodes[21])
+
+	addLeftSon(nodes[11], nodes[22])
+	addRightSon(nodes[11], nodes[23])
+
+	addLeftSon(nodes[12], nodes[24])
+	addRightSon(nodes[12], nodes[25])
+
+	addLeftSon(nodes[13], nodes[26])
+	addRightSon(nodes[13], nodes[27])
+
+	addLeftSon(nodes[14], nodes[28])
+	addRightSon(nodes[14], nodes[29])
+
+	addLeftSon(nodes[15], nodes[30])
+	addRightSon(nodes[15], nodes[31])
+
+	nodes[1].heapifyDown()
+
+	node2 := createNodes(32)
+
+	addLeftSon(node2[1], node2[2])
+	addRightSon(node2[1], node2[3])
+
+	addLeftSon(node2[2], node2[4])
+	addRightSon(node2[2], node2[5])
+
+	addLeftSon(node2[3], node2[6])
+	addRightSon(node2[3], node2[7])
+
+	addLeftSon(node2[4], node2[8])
+	addRightSon(node2[4], node2[9])
+
+	addLeftSon(node2[5], node2[11])
+	addRightSon(node2[5], node2[12])
+
+	addLeftSon(node2[6], node2[12])
+	addRightSon(node2[6], node2[13])
+
+	addLeftSon(node2[7], node2[14])
+	addRightSon(node2[7], node2[15])
+
+	addLeftSon(node2[8], node2[16])
+	addRightSon(node2[8], node2[17])
+
+	addLeftSon(node2[9], node2[18])
+	addRightSon(node2[9], node2[19])
+
+	addLeftSon(node2[10], node2[20])
+	addRightSon(node2[10], node2[21])
+
+	addLeftSon(node2[11], node2[22])
+	addRightSon(node2[11], node2[23])
+
+	addLeftSon(node2[12], node2[24])
+	addRightSon(node2[12], node2[25])
+
+	addLeftSon(node2[13], node2[26])
+	addRightSon(node2[13], node2[27])
+
+	addLeftSon(node2[14], node2[28])
+	addRightSon(node2[14], node2[29])
+
+	addLeftSon(node2[15], node2[30])
+	addRightSon(node2[15], node2[31])
+
+	if !reflect.DeepEqual(nodes[1], node2[1]) {
+		(*t).Errorf("heaps are not equals")
+	}
+}
+
 func TestHeapNode_heapifyUp(t *testing.T) {
 	type fields struct {
 		cell   *Cell
